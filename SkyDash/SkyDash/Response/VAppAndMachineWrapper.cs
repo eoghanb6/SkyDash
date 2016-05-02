f4d7131c073206ce1ae7a52ec09ba21fa004113f
eoghanb6 <eoghanbradley21@gmail.com>
2016-05-02 08:46:57 +0100

    Backup, VApp, VM and Wrapper classes added

---
+++ SkyDash/SkyDash/Response/VAppAndMachineWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Skydash.Response
+{
+    public class VAppAndMachineWrapper
+    {
+        [JsonProperty("vapps")]
+        public List<Dictionary<string, List<VirtualApp>>> VirtualApps { get; set; }
+
+        [JsonProperty("vms")]
+        public List<Dictionary<string, List<VirtualMachine>>> VirtualMachines { get; set; }
+    }
+}
