6b9de91a7f214458b7bad4fb082f7d54b883f1e8
eoghanb6 <eoghanbradley21@gmail.com>
2016-05-02 09:28:44 +0100

    fixed local files

--- SkyDash/Controllers/HomeController.cs
+++ SkyDash/Controllers/HomeController.cs
@@ -4,32 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RestSharp;
+using Newtonsoft.Json;
+using SkyDash.Skyscape.Response;
+using SkyDash.Skyscape;
 
 namespace SkyDash.Controllers
 {
     public class HomeController : Controller
     {
+      
         public ActionResult Index()
         {
-            var client = new RestClient("https://portal.skyscapecloud.com/api/authenticate");
-            client.CookieContainer = new System.Net.CookieContainer();
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("postman-token", "c7ea7240-427e-df9e-01e5-648428eb669c");
-            request.AddHeader("cache-control", "no-cache");
-            request.AddHeader("content-type", "application/json");
-            request.AddParameter("application/json", "{\n\"email\": \"email\",\n\"password\": \"pass\"\n}\n", ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
-            ViewBag.response = response.Content;
+            var authenticate = APIMethods.authenticate(Config.email, Config.password);
+            var accounts = APIMethods.getAccounts(Config.email, Config.password);
+            var vms = APIMethods.getVms(Config.email, Config.password);
 
-            client = new RestClient("https://portal.skyscapecloud.com/api/my_vm");
-            
-            request = new RestRequest(Method.GET);
-            request.AddHeader("postman-token", "124fc3f6-f149-1dac-9726-6b5c271db8ee");
-            request.AddHeader("cache-control", "no-cache");
-            request.AddHeader("content-type", "application/json");
-            request.AddParameter("application/json", "{\n\"email\": \"email\",\n\"password\": \"pass\"\n}\n", ParameterType.RequestBody);
-            IRestResponse response2 = client.Execute(request);
-            ViewBag.response2 = response2.Content;
+            ViewBag.response = authenticate.Content;
+            APIMethods.client.CookieContainer = new System.Net.CookieContainer();
+
+            //ViewBag.response2 = response2.Content;
+
+            //Account(VMs,Vapps)
+
+            // VDCs (List of VDCs)>>VDC(List of VMs)>>VM(VM Details)>>Backup(Backup Details)
+
+
+            // VDCs (List of VDCs)>>VDC(List of Vapps)>>Vapp(Vapp Details)
+                        
+            // LINQ
+
+
 
             return View();
         }
